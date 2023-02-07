18394ac99b8283fc38c8900708713310ee8fd1f8
sreya-kata <sreya.kata@gmail.com>
2023-02-06 20:51:18 -0500

    added console log and toast

--- src/components/GradeView.tsx
+++ src/components/GradeView.tsx
@@ -6,15 +6,30 @@ import {
   Editable,
   EditablePreview,
   EditableInput,
+  useToast,
 } from '@chakra-ui/react';
 import { CourseGrade } from '../types/transcript';
 
 export function GradeView({ grade }: { grade: CourseGrade }) {
+  const toast = useToast();
+
   return (
     <Stat>
       <StatLabel>{grade.course}</StatLabel>
       <StatNumber>
-        <Editable defaultValue={`${grade.grade}`} onSubmit={newValue => newValue}>
+        <Editable
+          defaultValue={`${grade.grade}`}
+          onSubmit={newValue => {
+            console.log(newValue);
+            toast({
+              title: 'Updated grade!',
+              description: `Grade is now ${newValue}`,
+              status: 'success',
+              duration: 9000,
+              isClosable: true,
+            });
+            return newValue;
+          }}>
           <EditablePreview />
           <EditableInput />
         </Editable>
